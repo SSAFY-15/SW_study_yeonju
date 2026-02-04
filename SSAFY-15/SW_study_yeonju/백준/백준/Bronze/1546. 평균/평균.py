9b4278571f57ae0b362232294b5603f5b74df065
LEE YEONJU <leeyj2306@naver.com>
2026-02-04 13:36:34 +0900

    Delete SSAFY-15/SW_study_yeonju/백준/백준/Bronze directory

--- SSAFY-15/SW_study_yeonju/백준/백준/Bronze/1546. 평균/평균.py
+++
@@ -1,9 +0,0 @@
-N = int(input())
-scores = list(map(int, input().split()))
-
-M = max(scores)
-
-for i in range(N):
-    scores[i] = scores[i] / M * 100
-    
-print(sum(scores) / N)
--- SSAFY-15/SW_study_yeonju/백준/백준/Bronze/2562. 최댓값/최댓값.py
+++
@@ -1,6 +0,0 @@
-nums = [int(input()) for _ in range(9)]
-
-max_num = max(nums)
-
-print(max_num)
-print(nums.index(max_num) + 1)
