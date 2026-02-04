fa7fc2f857001bc1c983cda959ae2225887e009c
LEE YEONJU <leeyj2306@naver.com>
2026-02-04 09:41:22 +0900

    Add string repetition functionality
    Implement string repetition based on input values.

---
+++ 백준/Bronze/2675. 문자열반복/문자열 반복.py
@@ -0,0 +1,13 @@
+T = int(input())
+
+
+for _ in range(T):
+    R, S = input().split()
+    R = int(R)
+    list = []
+
+    result = ''
+    for ch in S:
+        result += ch * R
+
+    print(result)
