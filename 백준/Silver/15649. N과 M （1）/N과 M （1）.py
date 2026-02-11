51b70bb8458357e303bde697672c067e9b8b3924
leeyj2306-cloud <leeyj2306@naver.com>
2026-02-11 14:32:20 +0000

    Update yeonju BOJ solutions

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,21 @@
+
+def backtracking():
+    if len(path) == M:  
+        print(*path)  
+        return
+
+    for i in range(1, N+1):
+        if visited[i] == 0:
+            visited[i] = 1
+            path.append(i)
+            backtracking()
+            path.pop()
+            visited[i] = 0
+
+
+N, M = map(int, input().split())
+
+visited = [0] * (N+1)
+path = []
+
+backtracking()
