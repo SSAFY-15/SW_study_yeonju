24818f8d3c646cd4f35de10093b6217d95ccd816
leeyj2306-cloud <leeyj2306@naver.com>
2026-02-15 04:32:54 +0000

    Update yeonju BOJ solutions

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,13 +3,12 @@ N = int(input())
 lst = list(map(int, input().split()))
 price = list(map(int, input().split()))
 
-total = lst[0] * price[0]
+min_price = price[0]
+total = 0
 
-for i in range(1, N-1):
-    if price[i] == min(price[1:N-1]):
-        total += sum(lst[1:]) * price[i]
-        break
-    else:
-        total += lst[i] * price[i]
+for i in range(N-1):
+    if price[i] < min_price:
+        min_price = price[i]
+    total += min_price * lst[i]
 
 print(total)
