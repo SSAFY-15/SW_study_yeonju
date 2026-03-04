09dc2025a58a48efda7f36461fc91272d546d4e3
leeyj2306-cloud <leeyj2306@naver.com>
2026-03-04 03:33:30 +0000

    Update yeonju BOJ solutions

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+best = 0
+
+for i in range(N - 2):
+    for j in range(i + 1, N - 1):
+        for k in range(j + 1, N):
+            total = cards[i] + cards[j] + cards[k]
+            
+            if total <= M:
+                best = max(best, total)
+
+print(best)
