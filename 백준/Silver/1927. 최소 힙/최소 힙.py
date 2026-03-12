f8d2289d00c70532e6e080eca0602d166cc9f700
leeyj2306-cloud <leeyj2306@naver.com>
2026-03-12 02:43:36 +0000

    Update yeonju BOJ solutions

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,17 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+pq = []
+
+for _ in range(N):
+    x = int(input())
+    
+    if x == 0:
+        if pq:
+            print(heapq.heappop(pq))
+        else:
+            print(0)
+    else:
+        heapq.heappush(pq, x)
